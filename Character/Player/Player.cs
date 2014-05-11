5aa3a3420ec0d9509314c25df8066e1be256d7e3
sensorreinigung <jens@richter-fam.de>
2014-05-11 19:20:47 +0200

    TryCameraBoundsForcing [not working]

--- Character/Player/Player.cs
+++ Character/Player/Player.cs
@@ -1,4 +1,6 @@
-﻿using KryptonEngine.Controls;
+﻿using KryptonEngine;
+using KryptonEngine.Controls;
+using KryptonEngine.Entities;
 using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
@@ -53,6 +55,28 @@ namespace HanselAndGretel.Data
 		public void CheckForAbility(Activity pAcitvity)
 		{
 			throw new System.NotImplementedException();
+		}
+
+		/// <summary>
+		/// Forced den Player in den Viewport der Camera.
+		/// </summary>
+		public void ForceInCameraViewport(Camera pCamera, List<Rectangle> pMoveArea)
+		{
+			Move(ForceInCameraViewportDelta(pCamera), pMoveArea);
+		}
+
+		/// <summary>
+		/// Gibt den Vector2 zurück den der Player bewegt werden muss um in die Camera geforced zu sein.
+		/// </summary>
+		public Vector2 ForceInCameraViewportDelta(Camera pCamera)
+		{
+			Vector2 TmpNewPosition = Vector2.Zero;
+			Rectangle TmpCameraViewport = new Rectangle((int)(pCamera.Position.X - EngineSettings.VirtualResWidth / 2 / pCamera.Scale), (int)(pCamera.Position.Y - EngineSettings.VirtualResHeight / 2 / pCamera.Scale), (int)(EngineSettings.VirtualResWidth / pCamera.Scale), (int)(EngineSettings.VirtualResHeight / pCamera.Scale));
+			if (CollisionBox.Left < TmpCameraViewport.Left) TmpNewPosition.X = TmpCameraViewport.Left;
+			if (CollisionBox.Top < TmpCameraViewport.Top) TmpNewPosition.Y = TmpCameraViewport.Top;
+			if (CollisionBox.Right > TmpCameraViewport.Right) TmpNewPosition.X = TmpCameraViewport.Right - CollisionBox.Width;
+			if (CollisionBox.Bottom > TmpCameraViewport.Bottom) TmpNewPosition.Y = TmpCameraViewport.Bottom - CollisionBox.Height;
+			return TmpNewPosition - Position;
 		}
 
 		#endregion
