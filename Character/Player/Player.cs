b04c1fff73af87c45a2fd40a64a2941697405f58
sensorreinigung <jens@richter-fam.de>
2014-05-12 02:06:20 +0200

    Aufgeräumt

--- Character/Player/Player.cs
+++ Character/Player/Player.cs
@@ -19,7 +19,7 @@ namespace HanselAndGretel.Data
 		protected float mSpeed;
 
 		//References
-		private Player rOtherPlayer;
+		protected Player rOtherPlayer;
 		
 
 		#endregion
@@ -62,6 +62,11 @@ namespace HanselAndGretel.Data
 			Move(ViewportCheckedVector(mInput.Movement * mSpeed), GetBodiesForCollisionCheck());
 		}
 
+		#region Update Movement Helper
+
+		/// <summary>
+		/// Get Scene.MoveArea + OtherPlayers CollisionBox as Rectangle-List.
+		/// </summary>
 		protected List<Rectangle> GetBodiesForCollisionCheck()
 		{
 			List<Rectangle> TmpMoveArea = new List<Rectangle>(rScene.MoveArea);
@@ -69,6 +74,9 @@ namespace HanselAndGretel.Data
 			return TmpMoveArea;
 		}
 
+		/// <summary>
+		/// Vector Checked for rCamera.GameScreen & MaxScaling (/rCamera.ViewportMaxDimension).
+		/// </summary>
 		protected Vector2 ViewportCheckedVector(Vector2 pMovement)
 		{
 			if (InCameraBounds(pMovement))
@@ -77,15 +85,18 @@ namespace HanselAndGretel.Data
 			}
 			Vector2 TmpMovementInBounds = Vector2.Zero;
 			int TmpSteps = (pMovement.X < pMovement.Y) ? (int)pMovement.Y : (int)pMovement.X;
-			for (int i = 0; i < TmpSteps; i++) //Move Player step für step weniger, bis er in den Camera Viewport passt.
+			for (int i = TmpSteps; i > 0; i--) //Move Player step für step weniger, bis er in den Camera Viewport passt.
 			{
-				TmpMovementInBounds = pMovement - (pMovement / TmpSteps) * i;
+				TmpMovementInBounds = (pMovement / TmpSteps) * i;
 				if (InCameraBounds(TmpMovementInBounds))
 					return TmpMovementInBounds;
 			}
 			return Vector2.Zero;
 		}
 
+		/// <summary>
+		/// Check for rCamera.GameScreen & MaxScaling (/rCamera.ViewportMaxDimension) Collision.
+		/// </summary>
 		protected bool InCameraBounds(Vector2 pMovement)
 		{
 			if (CollisionBox.X + pMovement.X < 0 || CollisionBox.Y + pMovement.Y < 0 || CollisionBox.Right + pMovement.X > rCamera.GameScreen.Right || CollisionBox.Bottom + pMovement.Y > rCamera.GameScreen.Bottom)
@@ -121,6 +132,8 @@ namespace HanselAndGretel.Data
 			Rectangle TmpRectangleToCheck = new Rectangle(TmpPlayerLeft.Left, TmpPlayerUp.Top, TmpPlayerRight.Right - TmpPlayerLeft.Left, TmpPlayerDown.Bottom - TmpPlayerUp.Top);
 			return (TmpRectangleToCheck.Width <= rCamera.ViewportMaxDimension.X && TmpRectangleToCheck.Height <= rCamera.ViewportMaxDimension.Y) ? true : false;
 		}
+
+		#endregion
 
 		public void CheckForAbility(Activity pAcitvity)
 		{
